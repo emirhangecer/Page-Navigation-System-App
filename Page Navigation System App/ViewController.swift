d5e8913fc5b6ee11a56ab61faf15f96618eb8111
emirhangecer <emirhan19072121@gmail.com>
2024-07-01 22:04:25 +0300

    Initial Commit

---
+++ Page Navigation System App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Page Navigation System App
+//
+//  Created by Emirhan on 1.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
