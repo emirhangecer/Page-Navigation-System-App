43eb2fbeb12061a983bde566c04b29dc122912a9
emirhangecer <emirhan19072121@gmail.com>
2024-07-01 22:20:00 +0300

    new VC

---
+++ Page Navigation System App/OyunEkraniVC.swift
@@ -0,0 +1,18 @@
+//
+//  OyunEkraniVC.swift
+//  Page Navigation System App
+//
+//  Created by Emirhan on 1.07.2024.
+//
+
+import UIKit
+
+class OyunEkraniVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+ 
+}
---
+++ Page Navigation System App/SonucEkraniVC.swift
@@ -0,0 +1,18 @@
+//
+//  SonucEkraniVC.swift
+//  Page Navigation System App
+//
+//  Created by Emirhan on 1.07.2024.
+//
+
+import UIKit
+
+class SonucEkraniVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+   
+}
